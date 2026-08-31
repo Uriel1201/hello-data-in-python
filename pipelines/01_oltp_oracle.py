8326b50b24c6cf1052d39616d073f23bbac03ae4
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-31 09:46:32 -0600

    Update 01_oltp_oracle.py

--- pipelines/01_oltp_oracle.py
+++ pipelines/01_oltp_oracle.py
@@ -15,7 +15,7 @@ def main(sql_file: str) -> None:
     logging.basicConfig(level=logging.INFO)
     print("Hello from 01_oltp_oracle.py!")
     with dbs.get_conn(ODB_USER, ODB_PASSWORD, ODB_DSN) as conn:
-        sql = get_query(f"SQL/OLTP/{sql_file}")
+        sql = get_query(f"oltp/{sql_file}")
         dbs.print_oracle(conn, sql)
 
 
