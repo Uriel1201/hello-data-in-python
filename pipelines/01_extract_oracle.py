b5b982e6f6ec10f7bd816102eede9ddadc6d1c05
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-31 12:23:14 -0600

    Update 01_extract_oracle.py

--- pipelines/01_extract_oracle.py
+++ pipelines/01_extract_oracle.py
@@ -5,7 +5,7 @@ from hello_data_in_python.database_setup.config import (
     ODB_PASSWORD,
     ODB_USER,
 )
-from utils.arrow import get_query
+from utils import get_query
 
 
 def main(sql_file: str) -> None:
