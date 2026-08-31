81424ce83a8fb0ce3b505f0ce0d2102b43f84212
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-31 13:58:11 -0600

    Refactor sql.py by removing unused components
    Removed MyArrowTable class and get_my_table function.

--- pipelines/utils/sql.py
+++ pipelines/utils/sql.py
@@ -1,13 +1,3 @@
-from dataclasses import dataclass
-from pathlib import Path
-import pyarrow as pa
-
-@dataclass
-class MyArrowTable:
-    table: pa.Table
-    alias: str
-
-
 # ============================================================
 # get_query:
 # params:
@@ -18,16 +8,3 @@ def get_query(filename: str) -> str:
             return file.read()
     except FileNotFoundError:
         raise FileNotFoundError(f"SQL file '{filename}' does not exist.")
-
-
-# ============================================================
-# get_my_table:
-# params:
-# ============================================================
-def get_my_table(arrow_file: Path) -> MyArrowTable:
-    path_ = str(arrow_file)
-    with pa.memory_map(path_, "rb") as source:
-        return MyArrowTable(
-            table=pa.ipc.open_file(source).read_all(),
-            alias=arrow_file.stem,
-        )
