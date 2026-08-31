8778e84e588f225ad381aea6e0cc22dae53cd0a6
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-08-31 09:47:57 -0600

    Update __init__.py

--- src/hello_data_in_python/utils/__init__.py
+++ src/hello_data_in_python/utils/__init__.py
@@ -1,4 +1,4 @@
 
-from .sql import get_query, get_my_table, dbapi_conn, dbapi_to_arrow, print_dbapi
+from .sql import get_query, dbapi_conn, dbapi_to_arrow, print_dbapi
 
-__all__ = ["get_query", "get_my_table", "dbapi_conn", "dbapi_to_arrow", "print_dbapi"]
+__all__ = ["get_query", "dbapi_conn", "dbapi_to_arrow", "print_dbapi"]
