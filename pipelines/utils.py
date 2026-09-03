485300b049be4658e03d21dfc7140f371562fe13
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-03 10:59:41 -0600

    Update utils.py

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -1,1 +1,7 @@
-
+def str_to_bool(value):
+    value = value.lower()
+    if value in ("true", "1", "yes"):
+        return True
+    if value in ("false", "0", "no"):
+        return False
+    raise ValueError(f"Invalid boolean value: {value}")
