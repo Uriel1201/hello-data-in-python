13704fd7cb2bea731eada330fc893c4f47eb8238
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-04 05:48:19 +0000

    Add data processing modules

--- pipelines/oltp_mysql.py
+++ pipelines/oltp_mysql.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def main(sql_file: str) -> None:
-    print("Hello from oltp_postgresql.py!")
+    print("Hello from oltp_mysql.py!")
     logging.basicConfig(level=logging.INFO)
     with dbapi_conn(URI_MYSQL, "mysql") as conn:
         sql = get_query(f"oltp/{sql_file}")
