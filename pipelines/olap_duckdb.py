21abe7775fc61b1272f8a0420f3b357fbcb65cf9
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-05 12:05:42 -0600

    Update greeting message in olap_duckdb.py

--- pipelines/olap_duckdb.py
+++ pipelines/olap_duckdb.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 def main(arrow_file: str, sql: str) -> None:
     logging.basicConfig(level=logging.INFO)
-    print("Hello from 01_olap_arrow.py!")
+    print("Hello from olap_arrow.py!")
     path = Path("data/arrow") / arrow_file
     my_table = olap.get_my_table(path)
     sql = Path("olap") / sql
