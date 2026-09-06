3dcdd4d51cdd743c91823e2ab692f7bda5e839db
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-06 00:54:38 +0000

    Add data processing modules

--- pipelines/dataframes/02_polars.py
+++ pipelines/dataframes/02_polars.py
@@ -4,31 +4,62 @@
 #     "polars>=1.44.1",
 # ]
 # ///
-import polars as pl
-from pathlib import Path
 import argparse
+from pathlib import Path
+
+import polars as pl
+
 
 def main(transactions: Path) -> None:
     print("Hello from 02_polars.py!")
     if transactions.exists():
         pl.Config.set_tbl_width_chars(60)
         print("****EAGER MODE****")
-        transactions = pl.read_ipc(
+        eager = pl.read_ipc(
             source=transactions,
             columns=["SENDER", "RECEIVER", "AMOUNT"],
             n_rows=10,
             use_pyarrow=False,
             memory_map=True,
         )
-        print(transactions)
-        senders = transactions.group_by(pl.col("SENDER").alias("user_id")).agg(pl.col("AMOUNT").sum().alias("sended_amount"))
+        print(eager)
+        senders = eager.group_by(pl.col("SENDER").alias("user_id")).agg(
+            pl.col("AMOUNT").sum().alias("sended_amount")
+        )
         print(senders)
-        receivers = transactions.group_by(pl.col("RECEIVER").alias("user_id")).agg(pl.col("AMOUNT").sum().alias("received_amount"))
+        receivers = eager.group_by(pl.col("RECEIVER").alias("user_id")).agg(
+            pl.col("AMOUNT").sum().alias("received_amount")
+        )
         print(receivers)
-        net_changes = senders.join(receivers, on="user_id", how="full", coalesce=True).fill_null(0)
+        net_changes = senders.join(
+            receivers, on="user_id", how="full", coalesce=True
+        ).fill_null(0)
+        print(net_changes)
+
+        print("****LAZY MODE****")
+        lazy = pl.scan_ipc(source=transactions)
+        net_changes = (
+            lazy.group_by(pl.col("SENDER").alias("user_id"))
+            .agg(pl.col("AMOUNT").sum().alias("sended_amount"))
+            .join(
+                lazy.group_by(pl.col("RECEIVER").alias("user_id")).agg(
+                    pl.col("AMOUNT").sum().alias("received_amount")
+                ),
+                on="user_id",
+                how="full",
+                coalesce=True,
+            )
+            .fill_null(0)
+            .select(
+                pl.col("user_id"),
+                (pl.col("received_amount") - pl.col("sended_amount")).alias(
+                    "net_changes"
+                ),
+            )
+        ).collect()
         print(net_changes)
     else:
-        raise FileNotFoundError(f"Path {arrow_file} does not exist")
+        raise FileNotFoundError(f"Path {transactions} does not exist")
 
 
 if __name__ == "__main__":
